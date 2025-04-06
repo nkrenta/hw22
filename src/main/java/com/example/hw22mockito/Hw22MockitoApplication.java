b19da4e2e63941014b9a089ad2506e22d300cdce
kit <nkrenta@inbox.ru>
2025-04-06 18:07:09 +0300

    Init

---
+++ src/main/java/com/example/hw22mockito/Hw22MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw22mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw22MockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw22MockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw22mockito/Hw22MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw22mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw22MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
